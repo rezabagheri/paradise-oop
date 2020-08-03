25ccaae062c235c9eb9791870b34ac36b662c114
Reza Bagheri (Rezo) <rezabagheri@gmail.com>
2020-08-03 09:56:33 +0430

    first commit

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * 
+ * @package Paradise
+ */
+
+ if ( file_exists( dirname(( __FILE__ ) .'/vendor/autoload.php' ) ) ) {
+     require_once( dirname( ( __FILE__ ) .'/vendor/autoload.php' ) );
+ }
+
